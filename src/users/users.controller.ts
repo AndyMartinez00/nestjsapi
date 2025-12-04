fcdd1a1fc760cd57ecf6ba344fe525db6af70d6a
Hi <multi.andres.martinez@gmail.com~>
2025-12-03 20:37:44 -0600

    👔 se pasa del users_controller la propiedad users a users_services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,12 +11,6 @@ interface User {
 //users controller
 @Controller('users')
 export class UsersController {
-  private users: User[] = [
-    { id: '1', name: 'John Doe', email: 'jdoe@test.com' },
-    { id: '2', name: 'Jane Smith', email: 'jsmith@test.com' },
-    { id: '3', name: 'Alice Johnson pentes', email: 'ajohnson@test.com' },
-    { id: '4', name: 'Alice finner', email: 'afinner@test.com' },
-  ];
   //GET /users
   @Get()
   //method to get all users
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [
+    { id: '1', name: 'John Doe', email: 'jdoe@test.com' },
+    { id: '2', name: 'Jane Smith', email: 'jsmith@test.com' },
+    { id: '3', name: 'Alice Johnson pentes', email: 'ajohnson@test.com' },
+    { id: '4', name: 'Alice finner', email: 'afinner@test.com' },
+  ];
+}
