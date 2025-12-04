cf27f27713d8848920571355b9c0d9b536ac6173
Hi <multi.andres.martinez@gmail.com~>
2025-12-04 11:43:18 -0600

    👔 se pasa de users_controller  los metodos  a users_service para  manejar el patron de una sola responsabilidad  getAllUsers, getUserById1, getUserById2 08-07-35

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,48 +1,48 @@
 import { Controller, Get, Param, Post, Body, NotFoundException, Delete, Put, UnprocessableEntityException, ForbiddenException } from '@nestjs/common';
 import { CreateUserDto, UpdateUserDto } from './user.dt';
+import { UsersService } from './users.service';
 
 //users controller
 @Controller('users')
 export class UsersController {
+  //injection of UsersService
+  //one instance of UsersService for this controller
+  constructor(private usersService: UsersService) {}
   //GET /users
   @Get()
   //method to get all users
   getAllUsers() {
     console.log('Fetching all users');
-    return this.users;
+    //call service method to get all users
+    return this.usersService.findAll();
   }
-
   //GET /users/1
   @Get('/1')
   //method to get user by ID 1
   getUserById1() {
     console.log('Fetching user with ID 1');
-    return this.users.find((user) => user.id === '1');
+    //call service method to get all users and find user with ID 1
+    //the  find is method of findAll() result  de un array users
+    return this.usersService.findAll().find((user) => user.id === '1');
   }
-
   //GET /users/2
   @Get('/2')
   //method to get user by ID 2
   getUserById2() {
     console.log('Fetching user with ID 2');
-    return this.users.find((user) => user.id === '2');
+    //call service method to get all users and find user with ID 1
+    //the  find is method of findAll() result  de un array users
+    return this.usersService.findAll().find((user) => user.id === '2');
   }
   //GET /users/id
   @Get(':id')
   //method to get user by dynamic ID
   getUserById(@Param('id') id: string) {
     console.log(`Fetching user with IN ID ${id}`);
-    const user = this.users.find((user) => user.id === id);
-    if (!user) {
-      throw new NotFoundException(`Usuario con id ${id} no encontrado`);
-    }
-    // Forbidden access to user with ID 3
-    if (user.id === '3') {
-      throw new ForbiddenException('No tienes permiso para acceder a este usuario.');
-    }
+    const user = this.usersService.findById(id);
     return user;
   }
-
+  /*
   //POST /users
   @Post()
   //method to create a new user
@@ -164,5 +164,5 @@ export class UsersController {
     }
 
     return false; // no hay duplicado
-  }
+  } */
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { User } from './user.model';
 
 @Injectable()
 export class UsersService {
+  //property to hold users data tiped as User array from user.model.ts
   private users: User[] = [
     { id: '1', name: 'John Doe', email: 'jdoe@test.com' },
     { id: '2', name: 'Jane Smith', email: 'jsmith@test.com' },
     { id: '3', name: 'Alice Johnson pentes', email: 'ajohnson@test.com' },
     { id: '4', name: 'Alice finner', email: 'afinner@test.com' },
   ];
+  //method to get all users
+  findAll() {
+    //if no users, throw error
+    if (!this.users) {
+      throw new NotFoundException('No users found');
+    }
+    return this.users;
+  }
+  //method to find user by ID
+  findById(id: string) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`Usuario con id ${id} no encontrado`);
+    }
+    // Forbidden access to user with ID 3
+    if (user.id === '3') {
+      throw new ForbiddenException('No tienes permiso para acceder a este usuario.');
+    }
+    return user;
+  }
 }
