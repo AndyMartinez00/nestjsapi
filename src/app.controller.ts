951020f725af4c646d9615cf5dbbf4c13838290c
Hi <multi.andres.martinez@gmail.com~>
2025-12-05 14:00:53 -0600

    👔 se lee configuracion de variables de etorno  .env 09-05-35

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 
@@ -8,11 +9,21 @@ export class AppController {
     //injection of AppService and UsersService
     private readonly appService: AppService,
     private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
   ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    //accessing a configuration value using ConfigService
+    const myVar = this.configService.get<string>('MY_VAR');
+    //calling method from AppService
+    const message = this.appService.getHello();
+    //logging values to the console
+    console.log(`Configuration value MY_VAR: ${myVar}`);
+    console.log(`Message from AppService: ${message}`);
+    //combining and returning the message
+    const united = `${message} ${myVar}`;
+    return united;
   }
   //GET /users-count
   @Get('users-count')
