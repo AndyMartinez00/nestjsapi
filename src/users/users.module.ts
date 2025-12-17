04428617eb2d71a438276d67579e167dc2e12ff7
Hi <multi.andres.martinez@gmail.com~>
2025-12-17 13:23:49 -0600

    👔 se agrega a users.modules UsersController, UsersService 10-05-35

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  //registra el controlador y el servicio en el módulo
+  controllers: [UsersController],
+  providers: [UsersService],
+  //expones el servicio para que otros módulos puedan usarlo
+  //exports: [UsersService],
+})
 export class UsersModule {}
