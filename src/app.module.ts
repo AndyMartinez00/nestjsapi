55f5223dd15c37f0ed8cef696fdfdad5002ac047
Hi <multi.andres.martinez@gmail.com~>
2025-12-22 13:51:09 -0600

    🔧 se configura modulo de TypeORM string conecction db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     //https://docs.nestjs.com/techniques/configuration#use-module-globally
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    //configure TypeORM module and database connection settings
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', //'192.168.1.22',
+      port: 5432,
+      username: 'blog_user',
+      password: 'blog_password',
+      database: 'my_blog_db',
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     UsersModule,
   ],
