c252317723c3c82eb9d3f7384f38b11f19f3eac6
Hi <multi.andres.martinez@gmail.com~>
2025-12-23 09:53:52 -0600

    🔧 se configura leendo archivo .env para extraer string de conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,29 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { EnvConfig } from './env.models';
 
 @Module({
   imports: [
     //https://docs.nestjs.com/techniques/configuration#use-module-globally
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env', // fuerza ruta del archivo de variables de entorno
     }),
     //configure TypeORM module and database connection settings
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost', //'192.168.1.22',
-      port: 5432,
-      username: 'blog_user',
-      password: 'blog_password',
-      database: 'my_blog_db',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService<EnvConfig>) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST', { infer: true }),
+        port: configService.get('POSTGRES_PORT', { infer: true }),
+        username: configService.get('POSTGRES_USER', { infer: true }),
+        password: configService.get('POSTGRES_PASSWORD', { infer: true }),
+        database: configService.get('POSTGRES_DB', { infer: true }),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
