3570e8d12e2715c221e3c267a9967515a6e8381c
Hi <multi.andres.martinez@gmail.com~>
2026-01-20 16:45:18 -0600

    🔧 elimina propiedades que no esten en el DTO evita campos fentasmas  y lanza errorr  whitelist: true,, forbidNonWhitelisted: true,  16-03-35

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //permite eliminar propiedades no definidas en los DTOs
+      whitelist: true,
+      //lanza un error si faltan propiedades requeridas en los DTOs
+      forbidNonWhitelisted: true,
+      //lanza un error si hay propiedades no definidas en los DTOs
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
