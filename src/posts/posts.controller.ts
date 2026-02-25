d081dd664b6e01db877b019643e90792b39e2fa1
Hi <multi.andres.martinez@gmail.com~>
2026-02-25 17:52:30 -0600

    👔 se quita de posts controller PostReponseDto del return, incluyendo la relación con el usuario y su perfil 18-06-35

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get, Post, Body, Param, Delete, Put, ParseIntPipe } from '@
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
-import { PostResponseDto } from './dto/post-response.dto';
-import { plainToInstance } from 'class-transformer';
 
 @Controller('posts')
 export class PostsController {
@@ -12,33 +10,37 @@ export class PostsController {
   @Post()
   async create(@Body() createPostDto: CreatePostDto) {
     const newPost = await this.postsService.create(createPostDto);
-    return plainToInstance(PostResponseDto, newPost, {
+    return newPost;
+    /*  return plainToInstance(PostResponseDto, newPost, {
       excludeExtraneousValues: true,
-    });
+    }); */
   }
 
   @Get()
   async findAll() {
     const posts = await this.postsService.findAll();
-    return plainToInstance(PostResponseDto, posts, {
+    return posts;
+    /* return plainToInstance(PostResponseDto, posts, {
       excludeExtraneousValues: true,
-    });
+    }); */
   }
 
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
     const post = await this.postsService.findOne(id);
-    return plainToInstance(PostResponseDto, post, {
+    return post;
+    /* return plainToInstance(PostResponseDto, post, {
       excludeExtraneousValues: true,
-    });
+    }); */
   }
 
   @Put(':id')
   async update(@Param('id', ParseIntPipe) id: number, @Body() updatePostDto: UpdatePostDto) {
     const updatedPost = await this.postsService.update(id, updatePostDto);
-    return plainToInstance(PostResponseDto, updatedPost, {
+    return updatedPost;
+    /* return plainToInstance(PostResponseDto, updatedPost, {
       excludeExtraneousValues: true,
-    });
+    }); */
   }
 
   @Delete(':id')
